5f9b5805499f244200d0ec1fc86f2d275626439b
AdComte <adrien.comte.69@gmail.com>
2015-06-15 12:01:53 +0200

    Merge origin/master
    Conflicts:
    	.gitignore
    	src/http/Client.java

--- src/http/HTTPClient.java
+++ src/http/HTTPClient.java
@@ -21,8 +21,16 @@ public class HTTPClient {
      */
     public static void main(String[] args) {
         try {
-            Client c = new Client(InetAddress.getByName("127.0.0.1"), 4000);
-            c.envoyer(new String("GET http://www.google.fr HTTP/1.0").getBytes("ascii"));
+//            Client c = new Client(InetAddress.getByName("127.0.0.1"), 4000);
+            Client c = new Client(InetAddress.getByName("64.15.116.217"), 80);
+            c.envoyer(new String("GET / HTTP/1.1\n" +
+"Host: 64.15.116.217:80\n" +
+"User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0\n" +
+"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\n" +
+"Accept-Language: fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3\n" +
+"Accept-Encoding: gzip, deflate\n" +
+"DNT: 1\n" +
+"Connection: keep-alive").getBytes("ascii"));
             c.reception();
         } catch (IOException ex) {
             Logger.getLogger(HTTPClient.class.getName()).log(Level.SEVERE, null, ex);
