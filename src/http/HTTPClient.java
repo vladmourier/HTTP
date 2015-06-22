72713425e0160e363e41b758431b2b0ea274725a
AdComte <adrien.comte.69@gmail.com>
2015-06-22 03:03:13 +0200

    client buggué dans la version console de la page web, il y a trop de texte après la balise </html> dans la version texte, il en manque pour arriver jusqu'à la balise ...

--- src/http/HTTPClient.java
+++ src/http/HTTPClient.java
@@ -7,12 +7,15 @@ package http;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,28 +30,41 @@ public class HTTPClient {
      */
     public static void main(String[] args) {
         try {
-            String s_url = "http://www.google.fr";
+            System.out.println("Saisir l'addresse de la page voulue");
+            byte[] address = new byte[1000];
+            System.in.read(address);
+            String s_url = new String(address);
+            FileWriter fp = new FileWriter(new File("E:/file.txt"), false);
+//            String s_url = "http://www.koreus.com";
             String post = "";
+            StringBuilder document = new StringBuilder();
             URL url = new URL(s_url);
-            URLConnection connection = (HttpURLConnection) url.openConnection();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setUseCaches(false);
             connection.setDoOutput(true);
-            BufferedOutputStream BOS = new BufferedOutputStream(connection.getOutputStream());
-            BOS.write(post.getBytes());
-            BOS.flush();
+            connection.setDoInput(true);
             
+            BufferedOutputStream BOS = new BufferedOutputStream(connection.getOutputStream());
             BufferedInputStream BIS = new BufferedInputStream(connection.getInputStream());
             byte[] buffer = new byte[1024];
-            BIS.read(buffer);
-            System.out.println("reçu : \n" + buffer);
+            
+            BOS.write(post.getBytes());
+            BOS.flush();
+            while(BIS.read(buffer) != -1)
+            {
+                fp.write(new String(buffer));
+                document.append(new String(buffer));
+            }
+            System.out.println(new String(document));
+            
+            System.out.println("\nrestant à afficher " + BIS.available());
             
             
-            
-            
-            
-//            Client c = new Client(InetAddress.getByName("www.google.fr"), 80);
+//            Client c = new Client(InetAddress.getByName("www.koreus.com"), 80);
 //            //GET /repertoire/page.html HTTP/1.1
 //            System.out.println(c.getIa_c().getHostAddress());
-//            c.envoyer(new String("GET / HTTP/1.0 \nHost: " + c.getIa_c().getHostAddress() + ":80\n").getBytes());
+//            c.envoyer(new String("GET / HTTP/1.1\r\nHost:" + c.getIa_c().getHostAddress() + ":80\n").getBytes());
 //            c.reception();
         } catch (IOException ex) {
             Logger.getLogger(HTTPClient.class.getName()).log(Level.SEVERE, null, ex);
